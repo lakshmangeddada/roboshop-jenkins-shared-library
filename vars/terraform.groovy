714641f3584546606d66ad2cf2521a26914ca064
Raghu K <raghuk.vit@gmail.com>
2023-04-03 08:35:22 +0530

    Update roboshop-jenkins-shared-library/

---
+++ vars/terraform.groovy
@@ -0,0 +1,23 @@
+def call() {
+  pipeline {
+    agent any
+
+    stages {
+
+      stage('Init') {
+        steps {
+          sh 'terraform init -backend-config=env-dev/state.tfvars'
+        }
+      }
+
+      stage('Apply') {
+        steps {
+          sh 'terraform apply -auto-approve -var-file=env-dev/main.tfvars'
+        }
+      }
+
+    }
+
+  }
+}
+
