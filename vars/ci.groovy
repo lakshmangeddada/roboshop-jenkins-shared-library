9fbb542296af070679b93a5923e6fa41759734a9
Raghu K <raghuk.vit@gmail.com>
2023-04-04 08:29:51 +0530

    Update roboshop-jenkins-shared-library/

--- vars/ci.groovy
+++ vars/ci.groovy
@@ -7,12 +7,7 @@ def call() {
       stage('Compile/Build') {
         steps {
           script {
-            if(app_lang == "nodejs") {
-              sh 'npm install'
-            }
-            if(app_lang == "maven") {
-              sh 'mvn package'
-            }
+            common.compile()
           }
         }
       }
