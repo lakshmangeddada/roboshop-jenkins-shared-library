5d4e11ce2e633cd4441eebd656803671c3ce77af
Raghu K <raghuk.vit@gmail.com>
2023-04-24 09:15:20 +0530

    Update roboshop-jenkins-shared-library/

--- vars/ci.groovy
+++ vars/ci.groovy
@@ -23,9 +23,6 @@ def call() {
         }
       }
 
-      println GTAG
-      println BRANCH_NAME
-
       if(env.GTAG != "true" && env.BRANCH_NAME != "main") {
         stage('Test Cases') {
           common.testcases()
